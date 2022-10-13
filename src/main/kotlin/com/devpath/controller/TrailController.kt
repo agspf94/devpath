b4dfda9a39451b0f443521258329b2b22c9044a5
Anderson Fantin <agspf94@gmail.com>
2022-10-13 14:52:02 -0300

    [agspf94] Trying to make heroku works

--- src/main/kotlin/com/devpath/controller/TrailController.kt
+++ src/main/kotlin/com/devpath/controller/TrailController.kt
@@ -1,18 +1,18 @@
-package com.devpath.controller
-
-import com.devpath.entity.Trail
-import com.devpath.repository.TrailRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("/trail")
-class TrailController(
-    private val trailRepository: TrailRepository
-) {
-    @GetMapping("/all")
-    fun getAllTrails(): MutableList<Trail> {
-        return trailRepository.findAll()
-    }
-}
+//package com.devpath.controller
+//
+//import com.devpath.entity.Trail
+//import com.devpath.repository.TrailRepository
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//@RestController
+//@RequestMapping("/trail")
+//class TrailController(
+////    private val trailRepository: TrailRepository
+//) {
+//    @GetMapping("/all")
+//    fun getAllTrails(): MutableList<Trail> {
+//        return trailRepository.findAll()
+//    }
+//}
--- src/main/kotlin/com/devpath/entity/Trail.kt
+++ src/main/kotlin/com/devpath/entity/Trail.kt
@@ -1,13 +1,13 @@
-package com.devpath.entity
-
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-
-@Entity
-data class Trail(
-    @Id
-    @GeneratedValue
-    var id: Long? = null,
-    var name: String? = null
-)
+//package com.devpath.entity
+//
+//import javax.persistence.Entity
+//import javax.persistence.GeneratedValue
+//import javax.persistence.Id
+//
+//@Entity
+//data class Trail(
+//    @Id
+//    @GeneratedValue
+//    var id: Long? = null,
+//    var name: String? = null
+//)
