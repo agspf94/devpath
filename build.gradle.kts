80c30beb6347ba6c4c1549001d5270abb1f5cfac
Anderson Fantin <agspf94@gmail.com>
2022-11-06 13:52:33 -0300

    [agspf94] Updating Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.4"
-	id("io.spring.dependency-management") version "1.0.14.RELEASE"
+	id("org.springframework.boot") version "2.7.5"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 
@@ -30,7 +30,7 @@ dependencies {
 	// Spring JPA
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-	// PostgreSQL
+	// Postgres
 	runtimeOnly("org.postgresql:postgresql")
 
 	// Testing
