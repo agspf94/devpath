aaadec19d540fdfadb5a52b46f591807c884068a
Anderson Fantin <agspf94@gmail.com>
2022-11-15 10:41:22 -0300

    [agspf94] New application tests to ensure the context can be loaded

--- src/test/kotlin/com/devpath/DevPathApplicationTests.kt
+++ src/test/kotlin/com/devpath/DevPathApplicationTests.kt
@@ -3,6 +3,10 @@ package com.devpath
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/*
+Make sure that the application context can start
+ */
+
 @SpringBootTest
 class DevPathApplicationTests {
 	@Test
