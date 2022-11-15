f1eee8d83a3be57c85340f3bdb352baa03ff83c4
Anderson Fantin <agspf94@gmail.com>
2022-11-14 23:04:11 -0300

    [agspf94] Fixing tests

---
+++ src/test/kotlin/com/devpath/DevPathApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.devpath
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevPathApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
