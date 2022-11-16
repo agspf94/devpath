2f62f70e9d44844c2e50907efee25b2212d8f461
Anderson Fantin <agspf94@gmail.com>
2022-11-16 18:02:06 -0300

    [agspf94] Trail Controller tests

---
+++ src/test/kotlin/com/devpath/controller/TrailControllerTest.kt
@@ -0,0 +1,230 @@
+package com.devpath.controller
+
+import com.devpath.constants.Constants
+import com.devpath.constants.Constants.Companion.TRAIL_ALREADY_EXISTS
+import com.devpath.constants.Constants.Companion.TRAIL_DELETED
+import com.devpath.constants.Constants.Companion.TRAIL_NOT_FOUND
+import com.devpath.dto.trail.response.DeleteTrailResponse
+import com.devpath.exception.ErrorMessage
+import com.devpath.exception.exceptions.EmptyTrailListException
+import com.devpath.exception.exceptions.TrailAlreadyExistsException
+import com.devpath.mocks.TrailMockProvider.Companion.getCreateTrailRequest
+import com.devpath.mocks.TrailMockProvider.Companion.getTrail
+import com.devpath.mocks.TrailMockProvider.Companion.getUpdateTrailRequest
+import com.devpath.service.TrailService
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(TrailController::class)
+class TrailControllerTest {
+    @MockBean
+    private lateinit var trailService: TrailService
+
+    @Autowired
+    private lateinit var trailController: TrailController
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `Trail controller should not be null`() {
+        assertNotNull(trailController)
+    }
+
+    @Test
+    fun `Should create a trail successfully`() {
+        val createTrailRequest = getCreateTrailRequest()
+        val trail = getTrail(id = 1)
+
+        `when`(trailService.create(createTrailRequest)).thenReturn(trail)
+
+        mockMvc.perform(
+                post("/trail/create")
+                    .accept(APPLICATION_JSON)
+                    .content(jacksonObjectMapper().writeValueAsString(createTrailRequest))
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(trail)))
+
+        verify(trailService, times(1)).create(createTrailRequest)
+    }
+
+    @Test
+    fun `Should fails while creating a trail with a name that already exists`() {
+        val createTrailRequest = getCreateTrailRequest()
+        val errorMessage = TRAIL_ALREADY_EXISTS + createTrailRequest.name
+
+        `when`(trailService.create(createTrailRequest)).thenAnswer { throw TrailAlreadyExistsException(errorMessage) }
+
+        mockMvc.perform(
+                post("/trail/create")
+                    .accept(APPLICATION_JSON)
+                    .content(jacksonObjectMapper().writeValueAsString(createTrailRequest))
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().is4xxClientError)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(ErrorMessage(errorMessage))))
+
+        verify(trailService, times(1)).create(createTrailRequest)
+    }
+
+    @Test
+    fun `Should read a trail successfully`() {
+        val trail = getTrail(id = 1)
+
+        `when`(trailService.read(trail.id!!)).thenReturn(trail)
+
+        mockMvc.perform(
+                get("/trail/${trail.id!!}")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(trail)))
+
+        verify(trailService, times(1)).read(trail.id!!)
+    }
+
+    @Test
+    fun `Should fails while reading a trail that doesn't exist`() {
+        val trail = getTrail(id = 1)
+        val errorMessage = TRAIL_NOT_FOUND + trail.id
+
+        `when`(trailService.read(trail.id!!)).thenAnswer { throw NoSuchElementException(errorMessage) }
+
+        mockMvc.perform(
+            get("/trail/${trail.id!!}")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().is4xxClientError)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(ErrorMessage(errorMessage))))
+
+        verify(trailService, times(1)).read(trail.id!!)
+    }
+
+    @Test
+    fun `Should read all trails list successfully`() {
+        val trailsList = listOf(getTrail(id = 1), getTrail(id = 2))
+
+        `when`(trailService.readAll()).thenReturn(trailsList)
+
+        mockMvc.perform(
+                get("/trail/all")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(trailsList)))
+
+        verify(trailService, times(1)).readAll()
+    }
+
+    @Test
+    fun `Should fails while reading all trails list because there aren't any`() {
+        val errorMessage = Constants.TRAIL_LIST_IS_EMPTY
+
+        `when`(trailService.readAll()).thenAnswer { throw EmptyTrailListException(errorMessage) }
+
+        mockMvc.perform(
+                get("/trail/all")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(ErrorMessage(errorMessage))))
+
+        verify(trailService, times(1)).readAll()
+    }
+
+    @Test
+    fun `Should update a trail successfully`() {
+        val updateTrailRequest = getUpdateTrailRequest(id = 1)
+        val trail = getTrail(id = 1)
+
+        `when`(trailService.update(updateTrailRequest)).thenReturn(trail)
+
+        mockMvc.perform(
+                patch("/trail/update")
+                    .accept(APPLICATION_JSON)
+                    .content(jacksonObjectMapper().writeValueAsString(updateTrailRequest))
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(trail)))
+
+        verify(trailService, times(1)).update(updateTrailRequest)
+    }
+
+    @Test
+    fun `Should fails while updating a trail that doesn't exist`() {
+        val updateTrailRequest = getUpdateTrailRequest(id = 1)
+        val errorMessage = TRAIL_NOT_FOUND + updateTrailRequest.name
+
+        `when`(trailService.update(updateTrailRequest)).thenAnswer { throw NoSuchElementException(errorMessage) }
+
+        mockMvc.perform(
+                patch("/trail/update")
+                    .accept(APPLICATION_JSON)
+                    .content(jacksonObjectMapper().writeValueAsString(updateTrailRequest))
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().is4xxClientError)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(ErrorMessage(errorMessage))))
+
+        verify(trailService, times(1)).update(updateTrailRequest)
+    }
+
+    @Test
+    fun `Should delete a trail successfully`() {
+        val trail = getTrail(id = 1)
+        val deleteTrailResponse = DeleteTrailResponse(trail, TRAIL_DELETED)
+
+        `when`(trailService.delete(trail.id!!)).thenReturn(deleteTrailResponse)
+
+        mockMvc.perform(
+                delete("/trail/delete/${trail.id!!}")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(deleteTrailResponse)))
+
+        verify(trailService, times(1)).delete(trail.id!!)
+    }
+
+    @Test
+    fun `Should fails while deleting a trail that doesn't exist`() {
+        val trail = getTrail(id = 1)
+        val errorMessage = TRAIL_NOT_FOUND + trail.id!!
+
+        `when`(trailService.delete(trail.id!!)).thenAnswer { throw NoSuchElementException(errorMessage) }
+
+        mockMvc.perform(
+                delete("/trail/delete/${trail.id!!}")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().is4xxClientError)
+            .andExpect(content().json(jacksonObjectMapper().writeValueAsString(ErrorMessage(errorMessage))))
+
+        verify(trailService, times(1)).delete(trail.id!!)
+    }
+}
---
+++ src/test/kotlin/com/devpath/mocks/TrailMockProvider.kt
@@ -0,0 +1,44 @@
+package com.devpath.mocks
+
+import com.devpath.dto.trail.request.CreateTrailRequest
+import com.devpath.dto.trail.request.UpdateTrailRequest
+import com.devpath.entity.Trail
+
+class TrailMockProvider {
+    companion object {
+        fun getTrail(id: Int): Trail {
+            return Trail(
+                id = id,
+                name = "name",
+                duration = 0,
+                description = "description",
+                averageSalary = "averageSalary",
+                jobs = mutableSetOf(),
+                topics = mutableSetOf()
+            )
+        }
+
+        fun getCreateTrailRequest(): CreateTrailRequest {
+            return CreateTrailRequest(
+                name = "name",
+                duration = 0,
+                description = "description",
+                averageSalary = "averageSalary",
+                jobs = mutableSetOf(),
+                topics = mutableSetOf()
+            )
+        }
+
+        fun getUpdateTrailRequest(id: Int): UpdateTrailRequest {
+            return UpdateTrailRequest(
+                id = id,
+                name = "name",
+                duration = 0,
+                description = "description",
+                averageSalary = "averageSalary",
+                jobsIds = mutableSetOf(),
+                topicsIds = mutableSetOf()
+            )
+        }
+    }
+}
